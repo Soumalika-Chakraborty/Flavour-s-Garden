b73b5cdea6f824140a0c5e949ff24a3c0ad49493
Sn3h42006 <snehabanik06@gmail.com>
2026-01-15 20:58:06 +0530

    Delete backend directory

--- backend/src/config/db.ts
+++
@@ -1,9 +0,0 @@
-import * as mysql from "mysql2/promise";
-
-export const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Soumalika@2004",
-  database: "flavourgarden",
-});
-
